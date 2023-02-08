9b445706140f55362ae7d07dba65a2bb05a82f11
Phamishan Packiaseelan <phamishan93@gmail.com>
2023-02-08 15:07:02 +0100

    storkredsopslag done(-ish)

--- storkredsopslag.js
+++ storkredsopslag.js
@@ -1,9 +1,16 @@
-function searchForStorkredsopslag(){
+function searchForStorkredsopslag() {
 
     let resData = document.getElementById("resData");
     let userInput = document.getElementById("userInput").value;
-    
+
     fetch('https://api.dataforsyningen.dk/storkredse/' + userInput)
-    .then((response) => response.json())
-    .then((data) => {resData.innerHTML = data.navn});
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.navn === undefined) {
+                console.log("Fejl - tallet findes ikke i databasen")
+            }
+            else {
+                resData.innerHTML = data.navn
+            }
+        });
 }
