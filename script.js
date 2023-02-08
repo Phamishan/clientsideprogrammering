1922bce86008781f9aac315f047c858e03e0e7cc
ItzPolarBearDK <70688416+ItzPolarBearDK@users.noreply.github.com>
2023-02-08 13:15:44 +0100

    Fetch/Post nummer search finished and working.

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ function httpGet() {
     let kommunerOut = document.getElementById("kommunerOut")
     fetch('https://api.dataforsyningen.dk/postnumre/' + userIn)
         .then((response) => response.json())
-        .then((data) => { console.log(data.kommuner)
-           postNrOut.innerHTML = data.navn
-           kommunerOut.InnerHTML = data.kommuner
+        .then((data) => {
+            let i = 0;
+            let s = "";
+            //s = s + data.kommuner[0].navn;
+            let k = 1;
+            console.log(data.kommuner[i].navn)
+            postNrOut.innerHTML = data.navn
+            while (i < data.kommuner.length) {
+                if(i < 1) {
+                    s = s + data.kommuner[0].navn;
+                }
+                if(i >= 1){
+                    s = s + ", " + data.kommuner[i].navn
+                }
+                i++;
+            }
+            kommunerOut.innerHTML = s;
         })
 }
